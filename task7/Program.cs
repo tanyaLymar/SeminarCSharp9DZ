01d398cdd0c41fe478a2de92aac8db08aaa296cc
Tatiana Lymar <tanya.lymar87@gmail.com>
2023-02-25 13:30:02 +0300

    Task7 solved

---
+++ task7/Program.cs
@@ -0,0 +1,10 @@
+﻿// Написать программу возведения числа А в целую стень B
+
+int PowerOfNumbers(int a, int b)
+{
+    if (b == 1) return a;
+    else if (b == 0) return 1;
+    else  return (a * PowerOfNumbers(a, b - 1));
+}
+
+Console.WriteLine(PowerOfNumbers(3,4));
