271f4440b39e65aceb0b78fd603a3e9753e5b898
Tatiana Lymar <tanya.lymar87@gmail.com>
2023-02-25 12:29:34 +0300

    Task4 solved

---
+++ task4/Program.cs
@@ -0,0 +1,16 @@
+﻿// Найти сумму элементов от M до N, N и M заданы
+
+int m = 5;
+int n = 0;
+
+
+
+int SumNumbersRec (int m, int n)
+{
+    if (m > n) return m + SumNumbersRec(m-1, n) ;
+    else return n;
+}
+
+Console.WriteLine(SumNumbersRec(m, n));
+
+
