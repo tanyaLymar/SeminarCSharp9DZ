082afb59a095545b935c3ff18e76b4ebb158e648
Tatiana Lymar <tanya.lymar87@gmail.com>
2023-02-25 11:08:50 +0300

    Task3 solved

---
+++ task3/Program.cs
@@ -0,0 +1,12 @@
+﻿// Показать натуральные числа от M до N, N и M заданы
+
+int m = 12;
+int n = 6;
+
+string NumbersRec (int m, int n)
+{
+    if (m >= n) return $"{m} " + NumbersRec(m-1,n) ;
+    else return String.Empty;
+}
+
+Console.WriteLine(NumbersRec(m, n));
