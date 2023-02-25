3d762d378b8acd37ccc26cbcf3981aa95fe5da28
Tatiana Lymar <tanya.lymar87@gmail.com>
2023-02-25 12:36:18 +0300

    Task5 solved

---
+++ task5/Program.cs
@@ -0,0 +1,13 @@
+﻿// Найти сумму цифр числа
+
+int n = 12315;
+
+
+
+int SumNumbersRec (int n)
+{
+    if (n < 10) return n;
+    else  return n % 10 + SumNumbersRec(n / 10);
+}
+
+Console.WriteLine(SumNumbersRec(n));
