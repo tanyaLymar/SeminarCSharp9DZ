2354291899fcba22028aa5859ca69acc4e8cd7b3
Tatiana Lymar <tanya.lymar87@gmail.com>
2023-02-25 13:06:07 +0300

    Task6 solved

---
+++ task6/Program.cs
@@ -0,0 +1,17 @@
+﻿// Написать программу вычисления функции Аккермана
+
+int FunctionAccerman(int m, int n) 
+
+{
+        if (m == 0) return n + 1; 
+        else if (n == 0 && m > 0)
+        {
+            return FunctionAccerman(m - 1, 1);
+        } 
+        else 
+        {
+            return FunctionAccerman(m - 1, FunctionAccerman(m, n - 1));
+        }
+    }
+
+Console.WriteLine(FunctionAccerman(3, 2));
