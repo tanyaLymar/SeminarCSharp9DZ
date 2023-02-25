37fb18b5d0042649553bb83c58917be1fb00b784
Tatiana Lymar <tanya.lymar87@gmail.com>
2023-02-25 10:44:49 +0300

    Task1 solved

---
+++ task1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Показать натуральные числа от 1 до N, N задано
+
+int n = 5;
+int a = 1;
+
+string NumbersRec (int a, int n)
+{
+    if (n >= a) return $"{a} " + NumbersRec(a+1,n) ;
+    else return String.Empty;
+}
+
+Console.WriteLine(NumbersRec(a,n));
